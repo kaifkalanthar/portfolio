7b74179791585601d59f03d09e8a7f348ae3d474
kaifkalanthar <77534625+kaifkalanthar@users.noreply.github.com>
2023-04-02 22:22:53 +0530

    hero-section

---
+++ src/component/heroSection.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import hero from '../external/heroimage.svg';
+
+const HeroSection = () => {
+    return (
+        <div className="herosection">
+            <div className="herosection-content">
+                <h1 className="hs-header">I'm Mohamed Kaif<br /><span className="hs-subheader">Front-end Developer</span></h1>
+                <p className="hs-content">I am passionate about becoming a
+                    software developer and believe that good software can make
+                    the world a better place. I am a self-learner
+                    and always enthusiastic to learn new things.</p>
+                <button className='button'>Projects</button>
+            </div>
+            <img className='hero-image' src={hero} alt="" />
+        </div>
+    );
+}
+
+export default HeroSection;
